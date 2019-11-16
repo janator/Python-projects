d74f96b22f4d9a6bc0d495ae512347cf68b3063e
rudykh <48880517+rudykh@users.noreply.github.com>
2019-11-16 12:40:44 +0300

    Create constants.py

---
+++ tic-tac-toe/constants.py
@@ -0,0 +1,35 @@
+# параметры поля
+field_w = 421
+field_h = 542
+field_size = 3
+v = "vika"
+
+# параметры клетки
+pole_w = 137
+pole_h = 137
+padx = 0
+pady = 110
+space = 5
+
+#параметры кнопки старт
+but_place_x = 290
+but_plce_y = 18
+but_width = 120
+
+
+#параметры статуса
+st_width = 45
+st_height = 2
+st_x = 70
+st_y = 60
+
+
+#параметры окна выбора хода
+frame_width = 150
+frame_height = 45
+frame_x = 10
+frame_y = 10
+
+#параметры комбобокс
+comb_x = 13
+comb_y = 25
